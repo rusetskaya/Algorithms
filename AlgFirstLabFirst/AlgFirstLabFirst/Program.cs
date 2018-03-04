d0c20d87ab5dd74acc76f818ad36526ff98fdc2d
Rita Rusetskaya <r.rusetskaya@gmail.com>
2018-03-04 18:37:43 +0300

    Add first and second tasks.

---
+++ AlgFirstLabFirst/AlgFirstLabFirst/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgFirstLabFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] profit = new int[] {5, 6, 5, 1};
+            int[] distances = new int[] {6, 7, 12, 14};
+
+            var billboards = new Billboard(distances, profit);
+            int maxProfit = billboards.CountMaxProfitSum();
+            Console.WriteLine(maxProfit);
+            Console.ReadLine();
+        }
+    }
+
+    class Billboard
+    {
+        private int[] distances;
+        private int[] profit;
+
+        public Billboard(int[] distances, int[] profit)
+        {
+            this.distances = distances;
+            this.profit = profit;
+        }
+
+        public int CountMaxProfitSum()
+        {
+            List<int> M = new List<int> {};
+            M.Add(0);
+            M.Add(profit[0]);
+
+            for (int i = 2; i < distances.Length; i++)
+            {
+                M.Add(Math.Max(profit[i] + M[CountE(i)], M[i - 1]));
+            }
+
+            return M[distances.Length-1];
+        }
+
+        private int CountE(int j)
+        {
+            int maxDist = j-1;
+
+            while (distances[j] - distances[maxDist] < 5)
+               maxDist -= 1;
+
+            return maxDist;
+        }
+    }
+}
---
+++ AlgFirstLabFirst/AlgFirstLabSec/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgFirstLabSec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 8, 6, 3, 6 };
+            GraphIndependentSet set = new GraphIndependentSet(array);
+
+            Console.WriteLine("Максимальный суммарный вес в независимом множестве: " + set.CountMax()[set.CountMax().Count - 1]);
+            Console.WriteLine("Путь: ");
+            foreach (int t in set.FindPath())
+            {
+                Console.WriteLine(t);
+            }
+
+            Console.ReadLine();
+        }
+
+        class GraphIndependentSet
+        {
+            private int[] array = new int[] { };
+            List<int> sumWeight = new List<int>();
+            List<int> path = new List<int>();
+
+            public GraphIndependentSet(int[] array)
+            {
+                this.array = array;
+            }
+
+            public List<int> CountMax()
+            {
+                //path.Add(0);
+                path.Add(array[0] > array[1] ? 0 : 1);
+
+                sumWeight.Add(0);
+                sumWeight.Add(array[0]);
+                sumWeight.Add(Math.Max(array[0], array[1]));
+
+                for (int i = 2; i < array.Length; i++)
+                {
+                    sumWeight.Add(Math.Max(sumWeight[i - 1] + array[i], sumWeight[i]));
+                    if (sumWeight[i - 1] + array[i] > sumWeight[i])
+                    {
+                        path.Add(i);
+                    }
+                }
+
+                return sumWeight;
+            }
+
+            public List<int> FindPath()
+            {
+                for (int i = path.Count - 1; i > 0; i--)
+                {
+                    if (path[i] - path[i - 1] < 2)
+                    {
+                        path.RemoveAt(i - 1);
+                    }
+                }
+
+                return path;
+            }
+        }
+    }
+}
